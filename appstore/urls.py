c11aca702aa62ce715bafb1ef19b6ace199722a2
Charles <baron.chua@obf.ateneo.edu>
2023-07-22 14:57:45 +0800

    fifth commit

--- appstore/urls.py
+++ appstore/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
         path('register/', views.registerPage, name="register"),
-	    path('login/', views.loginPage, name="login"),  
+	path('login/', views.loginPage, name="login"),  
         path('logout/', views.logoutUser, name="logout"),  
-	    path('', views.home, name="home"),
-    path("", views.starting_page, name="starting-page"),
+	path('', views.home, name="home"),
+   
     path('account/', views.accountSettings, name="account"),
     path('user/', views.userPage, name ="user-page"),
     path('customer/<str:pk_test>/', views.customer, name="customer"),
--- appstore/views.py
+++ appstore/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 from .filters import OrderFilter
 from django.contrib.auth.forms import UserCreationForm
 
-from django.contrib.auth import authenticate, login , logout
+from django.contrib.auth import authenticate, login, logout
 
 from django.shortcuts import render
 
@@ -84,54 +84,51 @@ all_products = [
   },
 ]
 
-def get_date(product):
-  return product['date']
+@unauthenticated_user
+def registerPage(request):
 
+	form = CreateUserForm()
+	if request.method == 'POST':
+		form = CreateUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			username = form.cleaned_data.get('username')
 
+			group = Group.objects.get(name='customer')
+			user.groups.add(group)
 
+			messages.success(request, 'Account was created for ' + username)
+
+			return redirect('login')
+		
+
+	context = {'form':form}
+	return render(request, 'appstore/register.html', context)
+
+@unauthenticated_user
+def loginPage(request):
+
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password =request.POST.get('password')
+
+		user = authenticate(request, username=username, password=password)
+
+		if user is not None:
+			login(request, user)
+			return redirect('home')
+		else:
+			messages.info(request, 'Username OR password is incorrect')
+
+	context = {}
+	return render(request, 'appstore/login.html', context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
 
 @login_required(login_url='login')
-@allowed_users(allowed_roles=['admin'])
-def products(request):
-  products = Product.objects.all()
-  return render(request, 'appstore/products.html', {'products':products})
-  
-  
-
-def product_detail(request, slug):
-  identified_product = next(product for product in all_products if product['slug'] == slug)
-  return render(request, "appstore/product_detail.html", {
-    "product": identified_product
-  })
-
-
-def registerPage(request): 
-	if request.user.is_authenticated:
-		return redirect('home')
-	else:
-		form = CreateUserForm()
-		if request.method == 'POST':
-			form = CreateUserForm(request.POST)
-			if form.is_valid():
-				form.save()
-				user = form.cleaned_data.get('username')
-				messages.success(request, 'Account was created for ' + user)
-
-				return redirect('login')
-			
-
-		context = {'form':form}
-		return render(request, 'appstore/register.html', context)    
-   
-def starting_page(request):
-  sorted_products = sorted(all_products, key=get_date)
-  latest_products = sorted_products[-3:]
-  return render(request, "store/index.html", {
-    "products": latest_products
-  })
-  
-   
-@login_required(login_url='login') 
+@admin_only
 def home(request):
 	orders = Order.objects.all()
 	customers = Customer.objects.all()
@@ -146,35 +143,37 @@ def home(request):
 	'total_orders':total_orders,'delivered':delivered,
 	'pending':pending }
 
-	return render(request, 'appstore/dashboard.html', context)   
+	return render(request, 'appstore/dashboard.html', context)
 
 def userPage(request):
-    context = {}
-    return render(request, 'appstore/user.html', context)
+	context = {}
+	return render(request, 'appstore/user.html', context)
 
-@unauthenticated_user  
-def loginPage(request):
-  
-      if request.method == 'POST':
-          username =  request.POST.get('username')
-          password =  request.POST.get('password')
-              
-          user = authenticate(request, username= username, password= password)
-              
-          if user is not None:
-              login(request, user)
-              return redirect('home')
-          else:
-                  messages.info(request, 'Username OR password is incorrect')
-                  
-          context = {}
-          return render(request, 'appstore/login.html', context)
-  
-def logoutUser(request):
-    logout(request)
-    return redirect('login')
-  
-  
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
+def products(request):
+	products = Product.objects.all()
+
+	return render(request, 'appstore/products.html', {'products':products})
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
+def customer(request, pk_test):
+	customer = Customer.objects.get(id=pk_test)
+
+	orders = customer.order_set.all()
+	order_count = orders.count()
+
+	myFilter = OrderFilter(request.GET, queryset=orders)
+	orders = myFilter.qs 
+
+	context = {'customer':customer, 'orders':orders, 'order_count':order_count,
+	'myFilter':myFilter}
+	return render(request, 'appstore/customer.html',context)
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer']) 
 def accountSettings(request):
     customer = request.user.customer
     form = CustomerForm(instance=customer)
@@ -184,37 +183,19 @@ def accountSettings(request):
       if form.is_valid():
           form.save()
       
-      
-      
-      
-      
-    context = {}
+        
+    context = {'form':form}
     return render(request, 'appstore/account_settings.html', context)
 
-def starting_page(request):
-  sorted_products = sorted(all_products, key=get_date)
-  latest_products = sorted_products[-3:]
-  return render(request, "store/index.html", {
-    "products": latest_products
+def get_date(product):
+  return product['date']
+
+
+def product_detail(request, slug):
+  identified_product = next(product for product in all_products if product['slug'] == slug)
+  return render(request, "appstore/product_detail.html", {
+    "product": identified_product
   })
-
-
-
-
-@login_required(login_url = 'login')
-@allowed_users(allowed_roles = ['admin'])
-def customer(request, pk_test):
-  customer = Customer.objects.get(id=pk_test)
-  
-  orders = customer.order_set.all()
-  order_count = orders.count()
-  
-  myFilter = OrderFilter(request.GET, queryset = orders)
-  orders = myFilter.qs 
-  
-  context = {'customer':customer, 'orders':orders, 'order_count': order_count, 'myFilter':myFilter}
-  return render(request, 'appstore/customer.html', context)
-
 
 
   
